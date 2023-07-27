f59822659e00fa6fb8a4d249636fc058467f619b
mateosCZ <vjb382@alumni.ku.dk>
2023-07-27 11:45:38 +0200

    add the iso_contour mesh

---
+++ cpp_src/isosurface_mesh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#include "isosurface_mesh.h"
+
+
